c00747ffc7110e35a345b8cf910e21290812449f
Lars Blumberg <lars.blumberg@talea.de>
2020-12-07 11:52:31 +0100

    Change ion-page class on button click

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonButton, IonContent, IonPage } from '@ionic/react';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [styleClass, setStyleClass] = useState('initial-class')
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+    <IonPage className={styleClass}>
+      <IonContent>
+        <IonButton onClick={() => setStyleClass('other-class')}>Add Class</IonButton>
       </IonContent>
     </IonPage>
   );
